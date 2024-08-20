6ff105db2d0d3b3d99400ddd4155c52f500f441d
denismel2024S <denismel@vt.edu>
2024-08-20 00:47:49 -0400

    scaffolding
    queue infrastructure

---
+++ DDWebsite/js/queue.js
@@ -0,0 +1,26 @@
+class queue{
+    constructor(){
+        this.riders=[];
+    }
+
+    enqueue(rider){
+        this.riders.push(rider);
+    }
+
+    dequeue(){
+        this.riders.pop();
+    }
+
+    isEmpty(){
+
+    }
+
+    peek(){
+
+    }
+
+    printQueue(){
+        
+    }
+}
+
