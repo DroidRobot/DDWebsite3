0b99b17a5405fe9a5ad004694e3bbf57f55015e9
wulffandrew <165091432+wulffandrew@users.noreply.github.com>
2024-08-20 02:04:23 -0400

    Car

---
+++ DDWebsite/js/car.js
@@ -0,0 +1,7 @@
+class Car {
+    constructor(brand, model, color){
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+    }
+}
--- DDWebsite/js/driver.js
+++ DDWebsite/js/driver.js
@@ -2,7 +2,7 @@ class Driver extends User {
     constructor(phoneNumber, name, location, car) {
       super(phoneNumber, name, location);
       this.drivingStatus = 'Not driving'; // Initially set to "Not driving". Could be either driving, not driving, or on break.
-      this.car = car; // Car object
+      this.car = null; // Drivers have to reconfirm their car each time they start driving. After they save the car the first time, we can have that car saved for the next time or they can switch.
       this.riderQueue = new Queue(); // Queue of riders
     }
   
@@ -15,6 +15,18 @@ class Driver extends User {
           console.log(`Invalid status: ${status}. Valid statuses are: ${validStatuses.join(', ')}.`);
         }
       }
+
+    // possible saved vehicle implementation
+    
+    updateCar(car) {
+        this.car = car;
+    }
+
+    // changing the driver's vehicle/ initial setup
+
+    updateCar(brand, model, color){
+        this.car = new Car(brand, model, car);
+    }
   
     addToFrontOfQueue(rider) {
       this.riderQueue.unshift(rider);
