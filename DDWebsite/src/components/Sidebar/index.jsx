7a311bd0107ad5e04a862bb6fdfebd27b1315511
wulffandrew <165091432+wulffandrew@users.noreply.github.com>
2024-08-20 03:29:50 -0400

    Revert "Update index.jsx"
    This reverts commit ca0b8bd5ea301aacc3bb12be805ad2b1e2acd891.

--- DDWebsite/src/components/Sidebar/index.jsx
+++ DDWebsite/src/components/Sidebar/index.jsx
@@ -2,55 +2,22 @@ import './index.css'
 
 const Sidebar = () => {
     return (
-        <aside id="sidebar" className="sidebar">
-            <ul className="sidebar-nav" id="sidebar-nav">
+        <div className = "navBar">
+            <ul className = "sideBar">
+                <li className="nav-item">
+                    <a className="nav-link "target = "_blank" href="https://youtube.com">
+                    <span>Drivers Dashboard</span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link " target = "_blank" href = "https://youtube.com">
+                    <span>Passengers Dashboard</span>
+                    </a>
+                </li>
                 
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="/driver-main-dashboard">
-                        <i className="bi bi-grid"></i>
-                        <span>Dashboard</span>
-                    </NavLink>
-                </li>
-
-                <li className="nav-item">
-                    <NavLink className="nav-link collapsed" to="/local-driver-active">
-                        <i className="ri-car-fill"></i>
-                        <span>Driver Dashboard</span>
-                    </NavLink>
-                </li>
-
-                <li className="nav-item">
-                    <NavLink className="nav-link collapsed" to="/local-user">
-                        <i className="ri-bus-fill"></i>
-                        <span>Rider Dashboard</span>
-                    </NavLink>
-                </li>
-
-                <li className="nav-heading">Pages</li>
-
-                <li className="nav-item">
-                    <NavLink className="nav-link collapsed" to="/user-profile">
-                        <i className="bi bi-person"></i>
-                        <span>Profile</span>
-                    </NavLink>
-                </li>
-
-                <li className="nav-item">
-                    <NavLink className="nav-link collapsed" to="/driver-faq">
-                        <i className="bi bi-question-circle"></i>
-                        <span>F.A.Q</span>
-                    </NavLink>
-                </li>
-
-                <li className="nav-item">
-                    <NavLink className="nav-link collapsed" to="/user-contact">
-                        <i className="bi bi-envelope"></i>
-                        <span>Contact</span>
-                    </NavLink>
-                </li>
-
             </ul>
-        </aside>
-    );
-};
+        </div>
+        
+    )
+}
 export default Sidebar;
