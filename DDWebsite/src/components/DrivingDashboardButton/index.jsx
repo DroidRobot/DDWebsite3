7c1775bb24a40f31aa16e3992d3875b92cefec99
Andrew Wulff <136127565+DroidRobot@users.noreply.github.com>
2024-08-20 23:57:13 -0400

    Minor stuff

---
+++ DDWebsite/src/components/DrivingDashboardButton/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const DrivingDashboardButton = () => {
+  return (
+    <>
+        <li className="nav-item">
+      <a className="nav-link" href="driver-main-dashboard.html">
+        <i className="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li>
+    {/* End Dashboard Nav */}
+    </>
+    
+  );
+};
+
+export default DrivingDashboardButton;
--- DDWebsite/src/components/Sidebar/index.jsx
+++ DDWebsite/src/components/Sidebar/index.jsx
@@ -14,7 +14,29 @@ const Sidebar = () => {
                     <span>Passengers Dashboard</span>
                     </a>
                 </li>
+                <li className="nav-heading">Pages</li>
+
+                <li className="nav-item">
+                <a className="nav-link collapsed" href="user-profile.html">
+                    <i className="bi bi-person"></i>
+                    <span>Profile</span>
+                </a>
+                </li>
+
+                <li className="nav-item">
+                <a className="nav-link collapsed" href="driver-faq.html">
+                    <i className="bi bi-question-circle"></i>
+                    <span>F.A.Q</span>
+                </a>
+                </li>
                 
+
+                <li className="nav-item">
+                <a className="nav-link collapsed" href="user-contact.html">
+                    <i className="bi bi-envelope"></i>
+                    <span>Contact</span>
+                </a>
+                </li>
             </ul>
         </div>
         
