5bc991260f712e5a34767404a7001c9a48ee402f
Andrew Wulff <136127565+DroidRobot@users.noreply.github.com>
2024-08-21 00:08:34 -0400

    recall

--- DDWebsite/src/App.jsx
+++ DDWebsite/src/App.jsx
@@ -2,13 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'boxicons/css/boxicons.min.css';
-import 'quill/dist/quill.snow.css';
-import 'quill/dist/quill.bubble.css';
-import 'remixicon/fonts/remixicon.css';
-import './assets/css/style.css';
 import './assets/js/main.js'
 import {Routes, Route} from 'react-router-dom'
 import  Main  from './components/Main'
