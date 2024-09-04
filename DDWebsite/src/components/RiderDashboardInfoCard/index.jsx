da9b9b7953df158c91b8b93573e0b3c7722a4485
Andrew Wulff <136127565+DroidRobot@users.noreply.github.com>
2024-09-04 19:55:06 -0400

    Update index.jsx

--- DDWebsite/src/components/RiderDashboardInfoCard/index.jsx
+++ DDWebsite/src/components/RiderDashboardInfoCard/index.jsx
@@ -1,19 +1,18 @@
 import './index.css'
 import React from 'react';
 
-const DashboardDriverInfoCard = () => {
+const RiderDashboardInfoCard = () => {
     return (
     
         <div className="col">
             <div className="card mb-4 rounded-3 shadow-sm">
                 <div className="card-header py-3">
-                <h4 className="my-0 fw-normal">{/* Title here */}</h4>
+                <h4 className="my-0 fw-normal">Driver Name</h4>
                 </div>
                 <div className="card-body">
-                <h2 className="card-title pricing-card-title">{/* Pricing title here */}</h2>
+                <h2 className="card-title pricing-card-title">People in Queue</h2>
                 <ul className="list-unstyled mt-3 mb-4">
-                    <li>{/* List item 1 */}</li>
-                    <li>{/* List item 2 */}</li>
+                    <li>Phone number</li>
                 </ul>
                 <button
                     className="btn btn-primary d-inline-flex align-items-center btn-join-queue"
@@ -28,4 +27,4 @@ const DashboardDriverInfoCard = () => {
   );
 };
 
-export default DashboardDriverInfoCard;
+export default RiderDashboardInfoCard;
